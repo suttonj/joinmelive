25ac09f56a790810202c2843383e1b84dc3700c8
Kyle Silberbauer <kyle.silberbauer@outlook.com>
2015-12-04 14:44:57 -0500

    landing page w/ jm icon

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
     render() {
-        return <div>join.me Live!</div>;
+        return (
+            <div style={styles.container}>
+                <div style={styles.innerContainer}>
+                    <img src="img/jm-logo.svg" style={styles.img} />
+                    <input type="text" />
+                </div>
+            </div>
+        );
     }
 }
+
+const styles = {
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    innerContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    img: {
+        width: 500,
+        height: 'auto',
+    },
+};
