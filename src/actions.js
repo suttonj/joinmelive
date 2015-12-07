ee85a9932c0a3e23f79793a07e3e0032219d21c4
Kyle Silberbauer <kyle.silberbauer@outlook.com>
2015-12-07 16:30:35 -0500

    using async/await for ajax action

--- src/actions.js
+++ src/actions.js
@@ -3,41 +3,20 @@ import {
 } from './constants';
 
 export function getCategories() {
-    return dispatch => {
-        fetch(API_HOST + '/category')
-            .then(res => res.json())
-            .then(categoriesJson => {
-                const proms = categoriesJson.map(category => 
-                    new Promise((resolve, reject) => 
-                        fetch(API_HOST + '/category?parentCategoryId=' + category.id)
-                            .then(res => res.json())
-                            .then(subCategoriesJson => resolve(subCategoriesJson)))
-                );
+    return async dispatch => {
+        const response = await fetch(API_HOST + '/category')
+        const categoriesJson = await response.json();
 
-                Promise.all(proms).then(subCategoriesJson => {
-                    const categories = categoriesJson.map((category, i) => {
-                        category.subCategories = subCategoriesJson[i];
-                        return category;
-                    });
+        const subCategoriesJson = await Promise.all(categoriesJson.map(async category => {
+            const subResponse = await fetch(API_HOST + '/category?parentCategoryId=' + category.id);
+            return await subResponse.json();
+        }));
 
-                    dispatch({ type: 'UPDATE_CATEGORIES', categories });
-                });
-            });
+        const categories = categoriesJson.map((category, i) => {
+            category.subCategories = subCategoriesJson[i];
+            return category;
+        });
+
+        dispatch({ type: 'UPDATE_CATEGORIES', categories });
     }
-    // return async dispatch => {
-    //     const response = await fetch(API_HOST + '/category');
-    //     const categoriesJson = await response.json();
-    //     const subCategoriesJson = await* categoriesJson.map(async category => {
-    //         const subResponse = await fetch(API_HOST + '/category?parentCategoryId=' + category.id);
-    //         const subCategories = await subResponse.json();
-    //         return subCategories;
-    //     });
-
-    //     const categories = categoriesJson.map((category, i) => {
-    //         category.subCategories = subCategoriesJson[i];
-    //         return category;
-    //     });
-
-    //     dispatch({ type: 'UPDATE_CATEGORIES', categories });
-    // };
 }
