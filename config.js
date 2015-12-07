e2e96f6c3892b51d7a92f104b50529ef8402df48
Jeremy Sutton <jeremy.s.sutton@gmail.com>
2015-12-07 16:34:42 -0500

    added search component with autocorrect and fuzzy match

--- config.js
+++ config.js
@@ -4,5 +4,8 @@ module.exports = {
     consumer_secret: 'ZiJ5mGUYtGHSmAhGH1SxknrHyCgGWXrDV5M37MP4gh07EIGG9y',
     access_token: '17788727-OHb64shg3jPtu8zPrMuPj7QOxKFagvSRaGWvOAxxJ',
     access_token_secret: 'a0VatADfB2waNHxRPjcbRdB2A0kMdOkbzqUPyBMiVVX6z'
+  },
+  server: {
+  	url: "http://localhost:3030/"
   }
 }
--- src/components/Landing/Trending.jsx
+++ src/components/Landing/Trending.jsx
@@ -28,35 +28,22 @@ export default class Trending extends Component {
         super(props);
 
         this.state = {
-            trends: []
+            trends: this.props.trends
         };
     }
-
-	componentWillMount() {
-		let self = this;
-		let request = new XMLHttpRequest();
-
-	    request.open('GET', apiUrl + 'trends', true);
-	    request.onload = () => {
-
-	    	if (request.status >= 200 && request.status < 400){
-	    		let trends = JSON.parse(request.responseText).trends;
-	    		trends = JSON.parse(trends)[0].trends;
-			    self.setState({trends: trends});
-			}
-		}
-
-	    request.send();
-	}
 
 	componentDidMount() {
 	    let self = this;
 	}
 
+	componentDidUpdate() {
+		this.setState({trends: this.props.trends});
+	}
+
 	render () {
 		var trendList = this.state.trends.map((trend) => {
 	        return (
-	            <Topic key={trend.tweet_volume} name={trend.name} />
+	            <Topic key={trend.query} name={trend.name} />
 	        )
 	    });
 
