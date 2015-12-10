dbf9155a79ce9b16e453c67aa66b5a74ec137b90
Kyle Silberbauer <kyle.silberbauer@outlook.com>
2015-12-10 10:55:27 -0500

    better categories drop down

--- src/components/Explore/CategoriesList.jsx
+++ src/components/Explore/CategoriesList.jsx
@@ -7,6 +7,7 @@ export default class CategoriesList extends Component {
 
         this.state = {
             isOpen: false,
+            hoveredCategoryId: null,
         };
 
         this.getCategory = this.getCategory.bind(this);
@@ -24,18 +25,27 @@ export default class CategoriesList extends Component {
         this.props.selectCategory(id);
     }
 
+
+
     render() {
         return (            
-            <div style={{position:'relative'}}
+            <div style={{position:'relative',cursor:'pointer'}}
                 onMouseOver={ () => this.setState({ isOpen: true }) }
                 onMouseOut={ () => this.setState({ isOpen: false }) }>
                 <span style={{color:'#f88300',borderBottom:'1px solid #f88300'}}>
-                    {this.getCategory()} ▼
+                    {this.getCategory()} <span style={{fontSize:'0.75em'}}>▼</span>
                 </span>
-                <div style={{position:'absolute',zIndex:1000,backgroundColor:'black',display:this.state.isOpen ? 'block' : 'none'}}>
-                    <div onClick={ () => this.selectCategory(null) }>{'<none>'}</div>
+                <div style={{position:'absolute',right:0,zIndex:1000,borderRadius:5,backgroundColor:'black',display:this.state.isOpen ? 'block' : 'none',padding:10,textAlign:'center',boxShadow:'0 0 11px 1px #111'}}>
+                    <div onClick={ () => this.selectCategory(null) } style={{color:'#9bd000',paddingTop:5}}><i>clear</i></div>
                 {this.props.categories.map(cat =>
-                    <div onClick={ () => this.selectCategory(cat.id) }>{cat.name}</div>
+                    <div
+                        style={{paddingTop:5,color:this.state.hoveredCategoryId === cat.id && '#F88300'}}
+                        key={cat.id}
+                        onClick={ () => this.selectCategory(cat.id) }
+                        onMouseOver={ () => this.setState({ hoveredCategoryId: cat.id }) }
+                        onMouseOut={ () => this.setState({ hoveredCategoryId: null }) }>
+                        {cat.name}
+                    </div>
                 )}
                 </div>
             </div>
